00cadc2e7080a1bf00e4690bd975ce63a0c40af3
BrigitteNikoll <BrigitteNikoll@users.noreply.github.com>
2021-04-15 19:54:30 -0500

    Minimizando función mdLinks

--- mdLinks.js
+++ mdLinks.js
@@ -1,34 +1,31 @@
-
 const {
   pathAbsolute,
   relativeToAbsolute,
   isValidRoute,
   gettingLinks,
   getAllFilesMd,
-  validateLinks
+  validateLinks,
+} = require('./index.js');
 
-} = require('./index.js')
-
-const mdlinks = (path, option = { validate: false}) => new Promise ((resolve, reject) => {
-
-  if(option.validate === true){
-    resolve(validateLinks(gettingLinks(path)))
+const mdlinks = (path, option = { validate: false }) => new Promise((resolve, reject) => {
+  if (option.validate === true) {
+    resolve(validateLinks(gettingLinks(path)));
   }
 
-  if(option.validate === false){
-    if(isValidRoute(path)){
-      if(!pathAbsolute(path)){
-        resolve(relativeToAbsolute(path))
+  if (option.validate === false) {
+    if (isValidRoute(path)) {
+      if (!pathAbsolute(path)) {
+        resolve(relativeToAbsolute(path));
       }
-        if(relativeToAbsolute(path)){
-          if(getAllFilesMd(path)){
-            resolve(gettingLinks(path))
-          }
+      if (relativeToAbsolute(path)) {
+        if (getAllFilesMd(path)) {
+          resolve(gettingLinks(path));
         }
-    }else{
-      reject("la ruta no existe")
+      }
+    } else {
+      reject("Esta ruta no existe");
     }
   }
-})
+});
 
 module.exports = mdlinks;
